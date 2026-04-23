2a59ae46d3cb88faaa2a664b2e1bc1044bad39e6
DANO <dcontrerasl@live.com>
2026-04-22 22:01:16 -0400

    fix: añadiendo imports a modulo de admin module

--- rifa-lovers-backend/src/modules/admin/admin.module.ts
+++ rifa-lovers-backend/src/modules/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AdminController } from './admin.controller'
 import { AdminService } from './admin.service'
 import { JobsModule } from '../jobs/jobs.module'
+import { DrawModule } from '../draw/draw.module'
 
 @Module({
-  imports: [JobsModule],
+  imports: [JobsModule, DrawModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
